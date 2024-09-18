894a04a3646141a770d5b754997df0b2e496fc3b
Elian Valdez <elian.valdez09@gmail.com>
2024-09-17 23:34:36 -0300

    uahsuahushas

--- src/lib/redis.js
+++
@@ -1,9 +0,0 @@
-import { Redis } from "@upstash/redis";
-
-const redis = new Redis({
-  url: "https://smashing-seal-61103.upstash.io",
-  token: "Ae6vAAIjcDE5OTBhMTZiN2ZlOWQ0ZDVmODI1YjI0NzExNWU1MmVkMnAxMA",
-});
-
-await redis.set("foo", "bar");
-const data = await redis.get("foo");
